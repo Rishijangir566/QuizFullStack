351c7e3e6e073383691e7506bf637e406cf2fe08
Rishijangir566 <rishijangirjr@gmail.com>
2025-02-10 13:32:47 +0530

    front add

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react"
+import instance from "./axiosConfig"
 
 function App() {
+
+  useEffect(()=>{
+  //  fetchQuestion()
+  },[])
+
+  async function fetchQuestion (){
+       const response = await instance.get("/quiz/question/get/programming")
+       console.log(response.data);
+       
+  }
+  async function secondpage (page){
+       const response = await instance.get(`/quiz/question/get/programming/?page=${page}`)
+       console.log(response.data);
+       
+  }
+
+
 
   return (
     <>
     <div className="text-3xl text-red-800">Hello</div>
-    
+       <button className="p-4 bg-red-200 mx-2" onClick={fetchQuestion}>1</button>
+       <button className="p-4 bg-red-200" onClick={()=>secondpage(2)}>2</button>
     </>
   )
 }
